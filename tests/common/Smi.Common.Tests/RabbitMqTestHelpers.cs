9e8dcfeac2406fbaaf1d28b7142fa05ee45a9fa3
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-10-01 18:14:30 +0100

    Add RabbitMqTestHelpers

---
+++ tests/common/Smi.Common.Tests/RabbitMqTestHelpers.cs
@@ -0,0 +1,86 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLog;
+using NUnit.Framework;
+using Smi.Common.Options;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+
+namespace Smi.Common.Tests
+{
+    /// <summary>
+    /// Helper class for managing RabbitMQ virtual hosts in tests
+    /// </summary>
+    public static class RabbitMqTestHelpers
+    {
+        private const int RABBITMQ_MANAGEMENT_PORT = 15672;
+        private const string TEST_VHOST_PREFIX = "smiservices-test-";
+
+        private static readonly HttpClient _client = new();
+
+        private static readonly ILogger _logger;
+
+
+        static RabbitMqTestHelpers()
+        {
+            TestLogger.Setup();
+            _logger = LogManager.GetLogger(typeof(RabbitMqTestHelpers).Name);
+
+            SetRabbitMqAuth("guest", "guest");
+        }
+
+        public static void SetRabbitMqAuth(RabbitOptions o) => SetRabbitMqAuth(o.RabbitMqUserName, o.RabbitMqPassword);
+
+        public static void SetRabbitMqAuth(string username, string password)
+        {
+            var byteArray = new UTF8Encoding().GetBytes($"{username}:{password}");
+            _client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(byteArray));
+        }
+
+        public static string CreateRandomVhost(string rabbitMqHostName)
+        {
+            var vhostName = $"{TEST_VHOST_PREFIX}{Guid.NewGuid().ToString().Split("-")[^1]}";
+
+            var response = _client.PutAsync($"http://{rabbitMqHostName}:{RABBITMQ_MANAGEMENT_PORT}/api/vhosts/{vhostName}", null).Result;
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode, "Expected a new vhost");
+
+            _logger.Debug($"Created vhost '{vhostName}'");
+            return vhostName;
+        }
+
+        public static IEnumerable<string> ListTestVhosts(string rabbitMqHostName)
+        {
+            var response = _client.GetAsync($"http://{rabbitMqHostName}:{RABBITMQ_MANAGEMENT_PORT}/api/vhosts/").Result;
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, "Expected a list of all vhosts");
+
+            using var streamReader = new StreamReader(response.Content.ReadAsStream());
+            var vhostList = JsonConvert.DeserializeObject<JArray>(streamReader.ReadToEnd());
+            return vhostList
+                .Select(v => (string)v["name"])
+                .Where(x => x.StartsWith(TEST_VHOST_PREFIX));
+        }
+
+        public static void DeleteVhost(string rabbitMqHostName, string vhostName)
+        {
+            var response = _client.DeleteAsync($"http://{rabbitMqHostName}:{RABBITMQ_MANAGEMENT_PORT}/api/vhosts/{vhostName}").Result;
+            Assert.AreEqual(HttpStatusCode.NoContent, response.StatusCode, "Expected to delete a vhost");
+
+            _logger.Debug($"Deleted vhost '{vhostName}'");
+        }
+
+        public static void DeleteAllTestVhosts(string rabbitMqHostName)
+        {
+            _logger.Debug("Deleting all test vhosts...");
+
+            foreach (var vhostName in ListTestVhosts(rabbitMqHostName))
+                if (vhostName.StartsWith(TEST_VHOST_PREFIX))
+                    DeleteVhost(rabbitMqHostName, vhostName);
+        }
+    }
+}
---
+++ tests/common/Smi.Common.Tests/RabbitMqTestHelpersTests.cs
@@ -0,0 +1,104 @@
+﻿using NUnit.Framework;
+using Smi.Common.Options;
+using System.Linq;
+
+namespace Smi.Common.Tests
+{
+    [RequiresRabbit]
+    public class RabbitMqTestHelpersTests
+    {
+        private RabbitOptions _rabbitOptions;
+
+        #region Fixture Methods
+
+        [OneTimeSetUp]
+        public void OneTimeSetUp()
+        {
+            TestLogger.Setup();
+
+            _rabbitOptions =
+               new GlobalOptionsFactory()
+               .Load(nameof(RabbitMqTestHelpersTests))
+               .RabbitOptions;
+        }
+
+        [OneTimeTearDown]
+        public void OneTimeTearDown() { }
+
+        #endregion
+
+        #region Test Methods
+
+        [SetUp]
+        public void SetUp()
+        {
+            RabbitMqTestHelpers.DeleteAllTestVhosts(_rabbitOptions.RabbitMqHostName);
+        }
+
+        [TearDown]
+        public void TearDown() { }
+
+        #endregion
+
+        #region Tests
+
+        [Test]
+        public void SetRabbitMqAuth_InvalidCredentials_Fails()
+        {
+            RabbitMqTestHelpers.SetRabbitMqAuth("foo", "bar");
+
+            Assert.Throws<AssertionException>(
+                () => RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName)
+            );
+        }
+
+        [Test]
+        public void CreateRandomVhost_HappyPath()
+        {
+            var vhostName = RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+            _rabbitOptions.RabbitMqVirtualHost = vhostName;
+
+            // Creates a test connection on construction
+            var _ = new RabbitMqAdapter(_rabbitOptions.CreateConnectionFactory(), "TestHost");
+        }
+
+        [Test]
+        public void ListTestVhosts_IncludesNew()
+        {
+            var before = RabbitMqTestHelpers.ListTestVhosts(_rabbitOptions.RabbitMqHostName).ToList();
+
+            var vhostName = RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+
+            var after = RabbitMqTestHelpers.ListTestVhosts(_rabbitOptions.RabbitMqHostName);
+
+            before.Add(vhostName);
+            Assert.True(before.OrderBy(x => x).SequenceEqual(after.OrderBy(x => x)));
+        }
+
+        [Test]
+        public void DeleteVhost_SpecificVhost()
+        {
+            var vhost1 = RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+            var vhost2 = RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+
+            RabbitMqTestHelpers.DeleteVhost(_rabbitOptions.RabbitMqHostName, vhost1);
+
+            var allVhosts = RabbitMqTestHelpers.ListTestVhosts(_rabbitOptions.RabbitMqHostName).ToList();
+            Assert.False(allVhosts.Contains(vhost1));
+            Assert.True(allVhosts.Contains(vhost2));
+        }
+
+        [Test]
+        public void DeleteAllTestVhosts_DeletesEverything()
+        {
+            RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+            RabbitMqTestHelpers.CreateRandomVhost(_rabbitOptions.RabbitMqHostName);
+
+            RabbitMqTestHelpers.DeleteAllTestVhosts(_rabbitOptions.RabbitMqHostName);
+
+            Assert.IsEmpty(RabbitMqTestHelpers.ListTestVhosts(_rabbitOptions.RabbitMqHostName));
+        }
+
+        #endregion
+    }
+}
