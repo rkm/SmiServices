c7f431f0053e14bff70b93ddaff829cca7ca5d80
Ruairidh MacLeod <ruairidh@rkm.dev>
2021-12-07 14:22:20 +0000

    Add xmldoc to IDicomAnonymiser

--- src/microservices/Microservices.DicomAnonymiser/Anonymisers/IDicomAnonymiser.cs
+++ src/microservices/Microservices.DicomAnonymiser/Anonymisers/IDicomAnonymiser.cs
@@ -5,6 +5,13 @@ namespace Microservices.DicomAnonymiser.Anonymisers
 {
     public interface IDicomAnonymiser
     {
+        /// <summary>
+        /// Anonymise the specified <paramref name="sourceFile"/> to <paramref name="destFile"></paramref>.
+        /// Implementations should assume that <paramref name="sourceFile"/> already exists, and <paramref name="destFile"></paramref> does not exist. 
+        /// </summary>
+        /// <param name="sourceFile"></param>
+        /// <param name="destFile"></param>
+        /// <returns></returns>
         ExtractedFileStatus Anonymise(IFileInfo sourceFile, IFileInfo destFile);
     }
 }
